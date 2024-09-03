bf8bb760171d0bf56137f3db145c94047963765a
pierre <dpierre2@ggc.edu>
2024-09-03 19:14:03 -0400

    Initial

---
+++ src/main/java/com/example/carwebapi/CarWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.carwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarWebApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/carwebapi/CarWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarWebApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
